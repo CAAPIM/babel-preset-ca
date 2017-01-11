3afc711400fb1f7d1b09ef6a93edace1491a2f3f
Bevan Hunt <bevan@bevanhunt.com>
2017-01-10 16:06:13 -0800

    feat: add tests

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var chalk = require('chalk');
+var babel = require('babel-core');
+var options = require('../index');
+
+babel.transformFile(`${process.cwd()}/test/input.js`, options, function (err, result) {
+  if (err) {
+    console.log(chalk.red('babel compliation failed:'));
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(chalk.green('babel complied without errors'));
+});
